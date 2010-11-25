fda0c05c15e396ba6f8e7056b547c8cf22d110f4
Jonathan Stasiak <jonathan.stasiak@gmail.com>
2010-11-25 10:44:18 -0500

    added dvc, updatd autocomplete to 1.3.1

--- local.el
+++ local.el
@@ -28,13 +28,17 @@
 (add-hook 'js2-mode-hook
           (lambda () (flymake-mode t)))
 
-;;auto-complete
-(vendor 'auto-complete)
-(global-auto-complete-mode t)
 
+;; yas
 (vendor 'yasnippet)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/snippets")
+
+;;auto-complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/ac-dict")
+(ac-config-default)
+(defun ac-css-mode-setup())
 
 ;;(vendor 'python-mode)
 (load-library "init_python")
@@ -42,9 +46,6 @@
 (autoload 'pep8 "python-pep8")
 (autoload 'python-pylint "python-pylint")
 (autoload 'pylint "python-pylint")
-
-(autoload 'css-complete "css-complete")
-(autoload 'css-check "css-check")
 
 ;; flymake shortcut
 (defun my-flymake-show-next-error()
@@ -61,3 +62,12 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/dvc/lisp/")
 (require 'dvc-autoloads)
 
+;; css-mode
+(add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
+(autoload 'css-mode "css-mode" nil t)
+;(autoload 'css-complete "css-complete")
+;(autoload 'css-check "css-check")
+
+
+;; randome file bindings
+(add-to-list 'auto-mode-alist '("\\.tmpl$" . html-mode))
